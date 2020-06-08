020593309dbad2a7dadc067c18040da1ecae64d3
alexvirga <alexander.virga@gmail.com>
2020-06-08 15:29:11 -0400

    Updates

---
+++ precache-manifest.20aff2a1b7743aa754b82aad47691c53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bcb6d79be87ecc0cc2c27c486a259f3",
+    "url": "/color-scheme/index.html"
+  },
+  {
+    "revision": "e899335fb11a7d098af3",
+    "url": "/color-scheme/static/css/main.5c9b1073.chunk.css"
+  },
+  {
+    "revision": "c1077271f32b00b2cee6",
+    "url": "/color-scheme/static/js/2.41eac954.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/color-scheme/static/js/2.41eac954.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e899335fb11a7d098af3",
+    "url": "/color-scheme/static/js/main.d607a22b.chunk.js"
+  },
+  {
+    "revision": "662f2ae69a64b11e2215",
+    "url": "/color-scheme/static/js/runtime-main.0242af25.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/color-scheme/static/media/logo.5d5d9eef.svg"
+  }
+]);
